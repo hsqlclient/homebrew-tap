7a94058833351cbc036fa6d77865e3da32207597
hsqlclient <hsqlclient+github@node163.com>
2024-09-10 07:34:38 +0000

    Update hsqlclient.rb

--- Formula/hsqlclient.rb
+++ Formula/hsqlclient.rb
@@ -2,6 +2,7 @@ class Hsqlclient < Formula
   desc "Client for HSQL"
   homepage "https://github.com/hsqlclient/hsqlclient"
   license "MIT"
+  revision 1
 
   if Hardware::CPU.arm?
     url "https://github.com/hsqlclient/hsqlclient/releases/download/0.9.0/hsqlclient-darwin-arm64.tar.gz"
@@ -12,6 +13,17 @@ class Hsqlclient < Formula
   end
 
   def install
+    cpu = Hardware::CPU.arm? ? "arm" : "intel"
+    system "env"
+
     bin.install "hsqlclient"
+  end
+
+  def post_install
+    output = `env`
+    puts output
+
+    ohai "hsqlclient has been installed!\n"
+    ohai "Usage: hsqlclient -h"
   end
 end
